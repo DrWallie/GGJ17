dadb03b6c09032da0515949dd3f6244799346d8a
JanVijfhuizen <Jan.vijfhuizen@outlook.com>
2017-01-20 23:50:22 +0100

    Gamemanager geupdated, playermanager aangemaakt.

---
+++ GlobalGameJam2017/Assets/_Scripts/Jan/PlayerManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerManager : NetworkBehaviour {
+
+    [HideInInspector]
+    public int thisID;
+
+    public override void OnStartLocalPlayer()
+    {
+        FFA_Manager.thisManager.AddPlayer(transform);
+    }
+}
