fafa8b712d39c32d4b1750be2342d69c46d31037
JanVijfhuizen <Jan.vijfhuizen@outlook.com>
2017-01-20 21:10:43 +0100

    Combat player updated
    Weapon/double weapon geschreven,
    equip / disequip geschreven
    initialize player first weapon geschreven.

---
+++ GlobalGameJam2017/Assets/_Scripts/Jan/PlayerCombat.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour {
+
+    public DoubleWeapon[] weapons;
+    public int equippedWeapon;
+
+    private void Awake()
+    {
+        InitializeFirstWeapon();
+    }
+
+    #region Equip / Disequip Weapon
+
+    private void InitializeFirstWeapon()
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            DoubleWeapon dW = weapons[i];
+            if (dW.ammo > 0)
+            {
+                EquipWeapon(i);
+                break;
+            }
+        }
+    }
+
+    public void EquipWeapon(DoubleWeapon dW)
+    {
+        EquipWeapon(false);
+        for(int i = 0; i < weapons.Length; i++)
+            if(weapons[i] == dW)
+            {
+                equippedWeapon = i;
+                break;
+            }
+        EquipWeapon(true);
+    }
+
+    public void EquipWeapon(int numIndex)
+    {
+        EquipWeapon(false);
+        equippedWeapon = numIndex;
+        EquipWeapon(true);
+    }
+
+    /// <summary>
+    /// dont directly use this, use one of the above
+    /// </summary>
+    private void EquipWeapon(bool equip)
+    {
+        weapons[equippedWeapon].thisWeapon.weaponTransform.gameObject.SetActive(equip);
+    }
+
+    #endregion
+}
---
+++ GlobalGameJam2017/Assets/_Scripts/Jan/Weapon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class WeaponStance {
+
+    public int damage;
+    [Tooltip("you use this to control the accuracy of the weapon, you random range between this for a shot.")]
+    public float offsetX, offsetY;
+    [Tooltip("the addforce the victim has when hit.")]
+    public float forceBullets;
+    public bool bouncingBullets;
+    public int bulletsPerShot;
+}
+
+[Serializable]
+public class DoubleWeapon
+{
+    public int ammo, maxAmmo;
+    public Weapon thisWeapon;
+    [Tooltip("Your two weapon stances.")]
+    public WeaponStance stance1, stance2;
+    [HideInInspector]
+    public WeaponStance currentStance;
+
+    public DoubleWeapon()
+    {
+        currentStance = stance1;
+    }
+}
+
+[Serializable]
+public class Weapon
+{
+    public Transform weaponTransform;
+    [Tooltip("This is where your bullets come from.")]
+    public Transform outputBullet;
+}
