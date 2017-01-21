6c0157c17ec809cb92bea824387f48fff785bde7
JanVijfhuizen <Jan.vijfhuizen@outlook.com>
2017-01-21 02:25:34 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	GlobalGameJam2017/Assets/_Prefabs/Test_Player.prefab

--- GlobalGameJam2017/Assets/_Scripts/Jan/PlayerManager.cs
+++ GlobalGameJam2017/Assets/_Scripts/Jan/PlayerManager.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+[RequireComponent(typeof(PlayerCombat), typeof(PlayerController))]
 public class PlayerManager : NetworkBehaviour {
 
     [SyncVar]
-    public int thisID;
+    public int thisID,
+        deaths,
+        kills;
     private GameManager gameManager;
+
+    [HideInInspector]
+    public PlayerCombat playerCombat;
+    [HideInInspector]
+    public PlayerController playerController;
+
+    private void Awake()
+    {
+        playerCombat = GetComponent<PlayerCombat>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    #region Game Manager
 
     public void Start()
     {
@@ -19,6 +35,34 @@ public class PlayerManager : NetworkBehaviour {
         if (isServer)
             gameManager.CmdAddToGameManager(thisID);
     }
-}
 
-       
+    [Command]
+    public void CmdOnKill()
+    {
+        kills++;
+        gameManager.CheckIfWon(thisID);
+    }
+
+    [Command]
+    public void CmdOnDeath()
+    {
+        deaths++;
+    }
+
+    #endregion
+
+    //this is purely visual for the clock locally
+    public IEnumerator Timer(int maxTime)
+    {
+        float timeLeft = maxTime;
+
+        while (timeLeft > 0f)
+        {
+            //show something visual
+            timeLeft -= Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        //this does not need to have consequences, you already do this in the gamemanager
+    }
+}
