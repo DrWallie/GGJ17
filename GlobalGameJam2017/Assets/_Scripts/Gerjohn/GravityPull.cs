2f1ff65454dead7d0aebe21636427c4b9816ae5e
Gerbor <gerjohnvandenbosch@hotmail.com>
2017-01-21 03:44:06 +0100

    Gravity greneade shitz

---
+++ GlobalGameJam2017/Assets/_Scripts/Gerjohn/GravityPull.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPull : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Pull(transform.position, 30f);
+	}
+
+    void Pull (Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        foreach (Collider c in hitColliders)
+        {
+            if(c.transform.tag == "Player")
+            {
+                c.transform.position = Vector3.Lerp(c.transform.position, transform.position, 5F * Time.deltaTime);
+            }
+        }
+    }
+}
